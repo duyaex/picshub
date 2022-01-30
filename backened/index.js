d9617cd68987761279a2a357813a3270dc2b6c0c
duyaex <oredd724@gmail.com>
2022-01-30 18:12:52 +0530

    prepare for deploy

--- backened/index.js
+++ backened/index.js
@@ -1,5 +1,6 @@
 const connectToMongo = require("./database")
-const express = require('express')
+const express = require('express');
+const path = require("path");
 
 connectToMongo();
 
@@ -11,7 +12,11 @@ app.use(express.json())
 
 app.use("/api/auth", require("./routes/auth"));
 
-
+//Server production assests
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join("../build")))
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")))
+}
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
